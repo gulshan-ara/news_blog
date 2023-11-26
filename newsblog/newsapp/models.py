d6cbf00786013b1dece98563f6cadcba1376ff02
gulshan-ara <gulshan3015@gmail.com>
2023-11-26 16:42:21 +0600

    page redesigned with somesorting logic

--- newsblog/newsapp/models.py
+++ newsblog/newsapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class BlogData(models.Model):
   blog_title = models.CharField(max_length=256, null=False, blank=False)
-  blog_content = models.CharField(max_length=2000, null=False, blank=False)
+  blog_content = models.TextField(null=False, blank=False)
   blog_author = models.CharField(max_length=256, null=False, blank=False)
   blog_creation_date = models.DateField()
   blog_image_primary = models.ImageField(upload_to="img/%y")
--- newsblog/newsapp/views.py
+++ newsblog/newsapp/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .models import *
 
 # Index function promised in urls
 def index(request):
-  # fetching values from blog table
-  blogs = BlogData.objects.all()
+  blogs_list = BlogData.objects.all().order_by('-blog_creation_date')
+  # Number of blogs to display per page
+  blogs_per_page = 2
+  # Get the current page from the request's GET parameters
+  page = request.GET.get('page', 1)
+  paginator = Paginator(blogs_list, blogs_per_page)
+
+  try:
+    blogs = paginator.page(page)
+  except PageNotAnInteger:
+    # If page is not an integer, deliver first page.
+    blogs = paginator.page(1)
+  except EmptyPage:
+    # If page is out of range (e.g. 9999), deliver last page of results.
+    blogs = paginator.page(paginator.num_pages)
   return render(request, 'newsapp/index.html', {'blogs' : blogs})
 
 def blog(request, pk):
